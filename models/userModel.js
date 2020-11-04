a8f9b15b0c51bc7af75fa05c87d91b13cbe7e00c
Chris Owens <guitarrockstarco@gmail.com>
2020-11-04 18:46:36 -0500

    added hashing to user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,98 +1,121 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
 
-const userSchema = mongoose.Schema(
-    {
-        firstName: {
-            type: String,
-            required: true
+const userSchema = mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    userName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    team: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Team',
+    },
+    parentForm: {
+        rider: {
+            firstName: String,
+            lastName: String,
         },
-        lastName: {
-            type: String,
-            required: true
-        },
-        userName: {
-            type: String,
-            required: true
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true
-        },
-        password: {
-            type: String,
-            required: true
-        },
-        isAdmin : {
-            type: Boolean,
-            default: false
-        },
-        team : {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Team'
-        },
-        parentForm : {
-            rider: {
-                firstName: String,
-                lastName: String
+        parentOne: {
+            firstName: String,
+            lastName: String,
+            phone: {
+                home: String,
+                cell: String,
             },
-            parentOne: {
-                firstName: String,
-                lastName: String,
-                phone: {
-                    home: String,
-                    cell: String
-                }
+        },
+        parentTwo: {
+            firstName: String,
+            lastName: String,
+            phone: {
+                home: String,
+                cell: String,
             },
-            parentTwo: {
-                firstName: String,
-                lastName: String,
-                phone: {
-                    home: String,
-                    cell: String
-                }
+        },
+        emergencyContactOne: {
+            firstName: String,
+            lastName: String,
+            phone: {
+                home: String,
+                cell: String,
             },
-            emergencyContactOne: {
-                firstName: String,
-                lastName: String,
-                phone: {
-                    home: String,
-                    cell: String
-                }
+        },
+        emergencyContactTwo: {
+            firstName: String,
+            lastName: String,
+            phone: {
+                home: String,
+                cell: String,
             },
-            emergencyContactTwo: {
-                firstName: String,
-                lastName: String,
-                phone: {
-                    home: String,
-                    cell: String,
-                }
-            }
         },
-        insurance: {
-            provider: String,
-            group: String,
-            number: String
-        },
-        medicalConditions: {
-            type: Boolean,
-            default: false
-        },
-        asthma: {
-            type: Boolean,
-            default: false
-        },
-        medicationRequired: {
-            type: Boolean,
-            default: false
-        },
-        ibprofenRelease: {
-            type: Boolean,
-            default: false
-        },
-        allergies: String
-    }
-);
+    },
+    insurance: {
+        provider: String,
+        group: String,
+        number: String,
+    },
+    medicalConditions: {
+        type: Boolean,
+        default: false,
+    },
+    asthma: {
+        type: Boolean,
+        default: false,
+    },
+    medicationRequired: {
+        type: Boolean,
+        default: false,
+    },
+    ibprofenRelease: {
+        type: Boolean,
+        default: false,
+    },
+    allergies: String,
+});
+
+userSchema.pre('save', function (next) {
+    const user = this;
+    if (!user.isModified('password')) return next();
+
+    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+        if (err) return next(err);
+
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err);
+
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+userSchema.methods.comparePassword = function (candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 const User = mongoose.model('User', userSchema);
 
