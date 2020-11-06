4f1ef9d9d2294cabb1f73dc873a97273b5bc6395
Justin Gardner <the1jscott@gmail.com>
2020-11-06 15:07:28 -0600

    wrote logic to upload & store user profile pic on server

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,54 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('../models/userModel');
+const multer = require('multer');
+const fs = require('fs');
+const { nextTick } = require('process');
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.query.userid + '.jpg')
+    }
+});
+let upload = multer({ storage: storage });
+const get_picture = (req, res)=> {
+    fs.readFile(`../server/uploads/${req.params.userid}.jpg`, (err, data) => {
+        if (err) {
+            fs.readFile('../uploads/default-user.jpg', (err, data2)=> {
+                if(err) {
+                    res.json(err);
+                }else {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.write('"data:image/jpeg;base64,')
+                    res.write(Buffer.from(data2).toString('base64'));
+                    res.end('"');
+                }
+            })
+        }else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write('"data:image/jpeg;base64,')
+            res.write(Buffer.from(data).toString('base64'));
+            res.end('"');
+        }
+    })
+}
+const upload_picture = () => {upload.single('file'), async(req, res) => {
+    if(req.query.userid) {
+        const file = req.file;
+        if (!file) {
+            const error = new Error("Please upload a file");
+            error.httpStatusCode = 400;
+            return nextTick(error);
+        }else {
+            res.send("file uploaded");
+        }
+    }else {
+        res.json("Error uploading");
+    }
+}
+}
 
 // Get All Users
 router.get('/', async (req, res) => {
@@ -40,5 +88,11 @@ router.delete(':/id', async (req, res) => {
     user.remove();
     res.json(user);
 });
+
+// ProfilePicture
+router.post('/UserProfile', upload_picture);
+router.get('/img/:userid', get_picture);
+
+
 
 module.exports = router;
